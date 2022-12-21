1612ebd1ae027b6d00085d892fed6d8448143022
XiangYuWAN <wanwf8@163.com>
2022-12-21 16:17:26 +0100

    add some explanatory note in main.py

--- main.py
+++ main.py
@@ -8,15 +8,21 @@ if __name__ == '__main__':
     yuvForm = "yuv420p"
     csvPath = "csvFile/basketballCU.csv"
 
-    Nomal_array = vpf.getVideoArray(videoPath, videoW, videoH, yuvForm)
+    # get 2D arrays of video with normal, after sobel, after lap, each row of arrays stand for a CU
+    Normal_array = vpf.getVideoArray(videoPath, videoW, videoH, yuvForm)
     sobel_array = vpf.getSobel_VideoArray(videoPath, videoW, videoH, yuvForm)
     lap_array = vpf.getLaplacian_VideoArray(videoPath, videoW, videoH, yuvForm)
 
-    N_result = vpf.Array2DTo_MSSK(Nomal_array)
+    # calculate each CU's mean, std, skew and kurt
+    N_result = vpf.Array2DTo_MSSK(Normal_array)
     S_result = vpf.Array2DTo_MSSK(sobel_array)
     L_result = vpf.Array2DTo_MSSK(lap_array)
+
+    # change column name
     S_result.columns = ['Sobel_Mean', 'Sobel_std', 'Sobel_Skew', 'Sobel_Kurt']
     L_result.columns = ['Lap_Mean', 'Lap_std', 'Lap_Skew', 'Lap_Kurt']
+
+    #  merge all these data frame together and produce a csv file
     result = pd.concat([N_result, S_result, L_result], axis=1)
     result.to_csv(csvPath)
     # print(result)
